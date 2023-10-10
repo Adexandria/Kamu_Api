e3c9de585c18c383c9bc0111d39e30963cf08269
Aderibigbe Adeola <115571361+Adeola-Aderibigbe@users.noreply.github.com>
2023-10-10 15:21:46 +0100

    Set up interfaces and Implementation for deployment and token configuration

---
+++ KAMU.API/Infrastructure/Utilities/DeploymentConfiguration.cs
@@ -0,0 +1,29 @@
+﻿namespace KAMU.API.Infrastructure.Utilities
+{
+    /// <summary>
+    /// Manages the configuration to set up API
+    /// </summary>
+    public class DeploymentConfiguration : IDeploymentConfiguration
+    {
+        /// <summary>
+        /// Super admin's first name
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// Super admin's last name
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// Super admin's email
+        /// </summary>
+        public string Email { get; set; }
+        /// <summary>
+        /// Super admin's password
+        /// </summary>
+        public string Password { get; set; }
+        /// <summary>
+        /// Super admin's username
+        /// </summary>
+        public string UserName { get; set; }
+    }
+}
---
+++ KAMU.API/Infrastructure/Utilities/TokenSecret.cs
@@ -0,0 +1,28 @@
+﻿namespace KAMU.API.Infrastructure.Utilities
+{
+    /// <summary>
+    /// Manages Token Secret
+    /// </summary>
+    public class TokenSecret : ITokenSecret
+    {
+        /// <summary>
+        /// Token issuer
+        /// </summary>
+        public string Issuer { get; set; }
+
+        /// <summary>
+        /// Token audience
+        /// </summary>
+        public string Audience { get; set; }
+
+        /// <summary>
+        /// Secret key
+        /// </summary>
+        public string SecretKey { get; set; }
+
+        /// <summary>
+        /// Expiration time in mins
+        /// </summary>
+        public int ExpiryTimeInMins { get; set; }
+    }
+}
