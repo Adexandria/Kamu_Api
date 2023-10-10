5c61927a09acdd2efa6650236961f4b72431bd69
Aderibigbe Adeola <115571361+Adeola-Aderibigbe@users.noreply.github.com>
2023-10-10 15:15:03 +0100

    Set up generic interface and implementation

--- KAMU.API/Infrastructure/Repositories/Interfaces/IRepository.cs
+++ KAMU.API/Infrastructure/Repositories/Interfaces/IRepository.cs
@@ -1,13 +1,32 @@
 ﻿using KAMU.API.Domain.Models;
-using KAMU.API.Infrastructure.Extensions;
+using KAMU.API.Infrastructure.Utilities;
 
 namespace KAMU.API.Infrastructure.Repositories.Interfaces
 {
+    /// <summary>
+    /// Manages the activities of all domain models.
+    /// </summary>
+    /// <typeparam name="T">Source type</typeparam>
     public interface IRepository<T> where T: IEntity
     {
+        /// <summary>
+        /// Adds source type to database
+        /// </summary>
+        /// <param name="entity">Source object</param>
         Task AddAsync(T entity);
+
+        /// <summary>
+        /// Updates source type in database
+        /// </summary>
+        /// <param name="entity">Source object</param>
         Task UpdateAsync(T entity);
+
+        /// <summary>
+        /// Deletes source type in the database
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
         Task DeleteAsync(T entity);
-        Task<ActionResponseResult> CommitAsync();
+       
     }
 }
--- KAMU.API/Infrastructure/Repositories/Repository.cs
+++ KAMU.API/Infrastructure/Repositories/Repository.cs
@@ -1,31 +1,54 @@
-﻿using KAMU.API.Infrastructure.Extensions;
+﻿using KAMU.API.Domain.Models;
+using KAMU.API.Infrastructure.Repositories.Interfaces;
 using ISession = NHibernate.ISession;
 namespace KAMU.API.Infrastructure.Repositories
 {
-    public class Repository
+    /// <summary>
+    /// Manages the activities of all domain models.
+    /// </summary>
+    /// <typeparam name="T">Source type</typeparam>
+    public class Repository<T> : IRepository<T> where T:IEntity
     {
+        /// <summary>
+        /// A constructor
+        /// </summary>
+        /// <param name="session">Manages the transactions in a database</param>
         public Repository(ISession session)
         {
             _session = session;
         }
 
-        public async Task<ActionResponseResult> CommitAsync()
+        /// <summary>
+        /// Adds source type to database
+        /// </summary>
+        /// <param name="entity">Source object</param>
+        public virtual async Task AddAsync(T entity)
         {
-            using var transaction = _session.BeginTransaction();
-            try
-            {
-                await transaction.CommitAsync();
-                await Task.CompletedTask;
-            }
-            catch (Exception ex)
-            {
-                return ActionResponseResult.Failed(ex.InnerException?.Message);
-            }
-
-            return ActionResponseResult.Successful();
+            await _session.SaveOrUpdateAsync(entity);
         }
 
+        /// <summary>
+        /// Updates source type in database
+        /// </summary>
+        /// <param name="entity">Source object</param>
+        public virtual async Task UpdateAsync(T entity)
+        {
+            await _session.UpdateAsync(entity);
+        }
 
+        /// <summary>
+        /// Deletes source type in the database
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public virtual async Task DeleteAsync(T entity)
+        {
+            await _session.DeleteAsync(entity);
+        }
+
+        /// <summary>
+        /// Manages the tansactions in the database
+        /// </summary>
         protected readonly ISession _session;
     }
 }
