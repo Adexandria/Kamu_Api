b1f843a5a7adea9ae0c36d03045a757f50faa1fc
Aderibigbe Adeola <115571361+Adeola-Aderibigbe@users.noreply.github.com>
2023-10-10 15:23:09 +0100

    Set up service to seed database

---
+++ KAMU.API/Infrastructure/Services/Deployment/IDeploymentService.cs
@@ -0,0 +1,17 @@
+﻿using KAMU.API.Infrastructure.Utilities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KAMU.API.Infrastructure.Services.Deployment
+{
+    /// <summary>
+    /// Manages the database seeding
+    /// </summary>
+    public interface IDeploymentService
+    {
+        /// <summary>
+        /// Seeds the super admin into the database
+        /// </summary>
+        /// <returns>Action response</returns>
+        Task<ActionResponse> CreateSuperAdmin();
+    }
+}
