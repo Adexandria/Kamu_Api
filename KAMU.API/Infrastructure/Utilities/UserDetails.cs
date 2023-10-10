59ce99b5b285f5d3045f50c810cc62bbbab38548
Aderibigbe Adeola <115571361+Adeola-Aderibigbe@users.noreply.github.com>
2023-10-10 15:22:41 +0100

    Set up model to handle user details

---
+++ KAMU.API/Infrastructure/Utilities/UserDetails.cs
@@ -0,0 +1,31 @@
+﻿using KAMU.API.Domain.Models;
+using KAMU.API.Infrastructure.Services.Authorisation;
+
+namespace KAMU.API.Infrastructure.Utilities
+{
+    /// <summary>
+    /// Handles the details of the user
+    /// </summary>
+    public class UserDetails
+    {
+        /// <summary>
+        /// A constructor
+        /// </summary>
+        /// <param name="_user">Handles the user</param>
+        /// <param name="_role">Handles the role of the user</param>
+        public UserDetails(Person _user, Role _role)
+        {
+            User = _user;
+            Role = _role;
+        }
+        /// <summary>
+        /// Handles the user
+        /// </summary>
+        public Person User { get; set; }
+
+        /// <summary>
+        /// Handles the role of the user
+        /// </summary>
+        public Role Role { get; set; }
+    }
+}
