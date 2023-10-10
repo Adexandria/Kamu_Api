464cec39b0b56239300fe3061d7e7eefd6d198ae
Aderibigbe Adeola <115571361+Adeola-Aderibigbe@users.noreply.github.com>
2023-10-10 15:24:16 +0100

    Add documentation

--- KAMU.API/Infrastructure/Extensions/IntExtensions.cs
+++ KAMU.API/Infrastructure/Extensions/IntExtensions.cs
@@ -1,21 +1,49 @@
 ﻿namespace KAMU.API.Infrastructure.Extensions
 {
+    /// <summary>
+    /// Manages the extensions of an integer type
+    /// </summary>
     public static class IntExtensions
     {
+        /// <summary>
+        /// Checks if the input is greater than the value
+        /// </summary>
+        /// <param name="input">Source object</param>
+        /// <param name="value">paramter object</param>
+        /// <returns>A boolean value</returns>
         public static bool IsGreaterThan(this int input, int value)
         {
             return input > value;
         }
 
+        /// <summary>
+        /// Checks if the input is less than the value
+        /// </summary>
+        /// <param name="input">Source object</param>
+        /// <param name="value">paramter object</param>
+        /// <returns>A boolean value</returns>
         public static bool IsLessThan(this int input, int value)
         {
             return !IsGreaterThan(input, value);
         }
 
+        /// <summary>
+        /// Checks if the input is greater than or equal to the value
+        /// </summary>
+        /// <param name="input">Source object</param>
+        /// <param name="value">paramter object</param>
+        /// <returns>A boolean value</returns>
         public static bool IsGreaterThanOrEqualTo(this int input, int value)
         {
             return input >= value;
         }
+
+        /// <summary>
+        /// Checks if the input is less than or equal to the value
+        /// </summary>
+        /// <param name="input">Source object</param>
+        /// <param name="value">paramter object</param>
+        /// <returns>A boolean value</returns>
         public static bool IsLessThanOrEqualTo(this int input, int value)
         {
             return input <= value;
