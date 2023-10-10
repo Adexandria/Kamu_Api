3c154e3a09feada525a3895504d9fc83136a6b9d
Aderibigbe Adeola <115571361+Adeola-Aderibigbe@users.noreply.github.com>
2023-10-10 15:11:45 +0100

    Set up extension to convert custom response to mvc response result

---
+++ KAMU.API/Infrastructure/Extensions/ActionResultExtension.cs
@@ -0,0 +1,42 @@
+﻿using KAMU.API.Infrastructure.Utilities;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace KAMU.API.Infrastructure.Extensions
+{
+    /// <summary>
+    /// Convert action response into mvc action result
+    /// </summary>
+    public static class ActionResultExtension
+    {
+        /// <summary>
+        /// Convert action response into mvc action result
+        /// </summary>
+        /// <param name="response">Handles request responses</param>
+        /// <returns>MVC Action Result</returns>
+        public static IActionResult ResponseResult(this ActionResponse response)
+        {
+            return response.StatusCode switch
+            {
+                StatusCodes.Status400BadRequest => new BadRequestObjectResult(response),
+                StatusCodes.Status404NotFound => new NotFoundObjectResult(response),
+                StatusCodes.Status401Unauthorized => new UnauthorizedObjectResult(response),
+                StatusCodes.Status500InternalServerError => new CustomObjectResult(response, StatusCodes.Status500InternalServerError),
+                StatusCodes.Status200OK => new OkObjectResult(response),
+                _ => new OkObjectResult(response)
+            };
+
+        }
+    }
+
+    /// <summary>
+    /// Creates an object result based on status code
+    /// </summary>
+    public class CustomObjectResult : ObjectResult
+    {
+        public CustomObjectResult(object? value, int statusCode) : base(value)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
