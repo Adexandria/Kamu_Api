089f557232051b1df127331eb0b614cfe6f7ad2f
Aderibigbe Adeola <115571361+Adeola-Aderibigbe@users.noreply.github.com>
2023-10-10 15:07:57 +0100

    Set up DTO for authentication

---
+++ KAMU.API/Domain/DTOs/LoginDetails.cs
@@ -0,0 +1,12 @@
+﻿namespace KAMU.API.Domain.DTOs
+{
+    public class LoginDetails
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public Guid OrganisationId { get; set; }
+    }
+}
