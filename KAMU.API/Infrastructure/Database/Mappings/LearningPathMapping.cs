a8bebed3702ab4ca1cfd9bb35a7be3adfd45f4a4
Aderibigbe Adeola <115571361+Adeola-Aderibigbe@users.noreply.github.com>
2023-10-10 15:11:13 +0100

    Refactor mapping

---
+++ KAMU.API/Infrastructure/Database/Mappings/LearningPathMapping.cs
@@ -0,0 +1,24 @@
+﻿using KAMU.API.Domain.Models;
+
+namespace KAMU.API.Infrastructure.Database.Mappings
+{
+    public class LearningPathMapping : BaseEntityMapping<LearningPath>
+    {
+        public LearningPathMapping()
+        {
+            Table("LearningPaths");
+            HasManyToMany(s => s.Instructors).
+                Table("LearningPathToInstructor")
+                .ParentKeyColumn("LearningPath_id")
+                .ChildKeyColumn("Instructor_id");
+            HasManyToMany(s=>s.SuperAdmins)
+                .Table("LearningPathToSuperAdmin")
+                 .ParentKeyColumn("LearningPath_id")
+                .ChildKeyColumn("SuperAdmin_id");
+            HasManyToMany(s=>s.Students)
+                .Table("LearningPathToStudent")
+                 .ParentKeyColumn("LearningPath_id")
+                .ChildKeyColumn("Student_id");
+        }
+    }
+}
---
+++ KAMU.API/Infrastructure/Database/Mappings/OrganisationMapping.cs
@@ -0,0 +1,15 @@
+﻿using KAMU.API.Domain.Models;
+
+namespace KAMU.API.Infrastructure.Database.Mappings
+{
+    public class OrganisationMapping : BaseEntityMapping<Organisation>
+    {
+        public OrganisationMapping()
+        {
+            Table("Organisations");
+            HasMany(s => s.Instructors);
+            HasMany(s => s.SuperAdmins);
+            HasMany(s => s.Students);
+        }
+    }
+}
