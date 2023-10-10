daf943d74c020c11e4f0625b6108543bb676a8f0
Aderibigbe Adeola <115571361+Adeola-Aderibigbe@users.noreply.github.com>
2023-10-10 15:20:35 +0100

    Set up security services to manage passwords, token and authentication

--- KAMU.API/Infrastructure/Services/Security/IAccessManager.cs
+++ KAMU.API/Infrastructure/Services/Security/IAccessManager.cs
@@ -1,7 +1,19 @@
-﻿namespace KAMU.API.Infrastructure.Services.Security
+﻿using KAMU.API.Infrastructure.Extensions;
+using KAMU.API.Infrastructure.Utilities;
+
+namespace KAMU.API.Infrastructure.Services.Security
 {
+    /// <summary>
+    /// Manages the authentication
+    /// </summary>
     public interface IAccessManager
     {
-        bool AuthenticateAsync(string email, string password);
+        /// <summary>
+        /// Authenticates user using email and password
+        /// </summary>
+        /// <param name="email">User's email</param>
+        /// <param name="password">User's password</param>
+        /// <returns>Action response</returns>
+        ActionResponse<UserDetails> AuthenticateUser(string email, string password);
     }
 }
---
+++ KAMU.API/Infrastructure/Services/Security/IUserContext.cs
@@ -0,0 +1,22 @@
+﻿namespace KAMU.API.Infrastructure.Services.Security
+{
+    /// <summary>
+    /// Manages the user details gotten from the claims
+    /// </summary>
+    public interface IUserContext
+    {
+        /// <summary>
+        /// User's id
+        /// </summary>
+        Guid CurrentUserId { get; set; }
+        /// <summary>
+        /// User's organisation id
+        /// </summary>
+        Guid OrganisationId { get; set; }
+
+        /// <summary>
+        /// User's email
+        /// </summary>
+        string Email { get; set; }
+    }
+}
