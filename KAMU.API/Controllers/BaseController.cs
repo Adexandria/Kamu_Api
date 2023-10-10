43038d38fcbc456096995880dadffb3dee8f99bd
Aderibigbe Adeola <115571361+Adeola-Aderibigbe@users.noreply.github.com>
2023-10-10 15:24:39 +0100

    Set up organisation and authentication controller

--- KAMU.API/Controllers/BaseController.cs
+++ KAMU.API/Controllers/BaseController.cs
@@ -1,11 +1,46 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using KAMU.API.Infrastructure.Services.Security;
+using KAMU.API.Infrastructure.Utilities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KAMU.API.Controllers
 {
+    /// <summary>
+    /// Inject dependencies
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class BaseController : ControllerBase
     {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="userContext">Manages the user details gotten from the claims</param>
+        /// <param name="validation">Validates request properties</param>
+        public BaseController(IUserContext userContext, Validation validation)
+        {
+            CurrentOrganisationId = userContext.OrganisationId;
+            CurrentUserEmail = userContext.Email;
+            CurrentUserId = userContext.CurrentUserId;
+            Validator = validation;
+        }
+        /// <summary>
+        /// Organisation id
+        /// </summary>
+        protected Guid CurrentOrganisationId { get; set; }
+
+        /// <summary>
+        /// User's email
+        /// </summary>
+        protected string CurrentUserEmail { get; set; }
+
+        /// <summary>
+        /// User's id
+        /// </summary>
+        protected Guid CurrentUserId { get; set; }
+
+        /// <summary>
+        /// Validates request properties
+        /// </summary>
+        protected Validation Validator { get; set; }
     }
 }
---
+++ KAMU.API/Controllers/OrganisationController.cs
@@ -0,0 +1,50 @@
+﻿using KAMU.API.Infrastructure.Extensions;
+using KAMU.API.Infrastructure.Services.Authorisation;
+using KAMU.API.Infrastructure.Services.Organization;
+using KAMU.API.Infrastructure.Services.Security;
+using KAMU.API.Infrastructure.Utilities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KAMU.API.Controllers
+{
+    /// <summary>
+    /// Handles organisations
+    /// </summary>
+    [Route("api/organisations")]
+    [ApiController]
+    [HasRole(Role.SuperAdmin)]
+    public class OrganisationController : BaseController
+    {
+        /// <summary>
+        /// A constructor
+        /// </summary>
+        /// <param name="userContext">Manages the user details gotten from the claims</param>
+        /// <param name="organisationService"> Manages the organisation services</param>
+        /// <param name="validation">Validates request properties</param>
+        public OrganisationController(IUserContext userContext, 
+            IOrganisationService organisationService,
+            Validation validation) : base(userContext, validation)
+        {
+            _organisationService = organisationService;
+        }
+
+        /// <summary>
+        /// Creates a new organisation
+        /// </summary>
+        /// <returns>Action response</returns>
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(ActionResponse<Guid>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ActionResponse<Guid>), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ActionResponse<Guid>), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ActionResponse<Guid>), StatusCodes.Status500InternalServerError)]
+        [HttpPost]
+        public async Task<IActionResult> CreateOrganisation()
+        {
+            var response = await _organisationService.CreateOrganisation();
+            return response.ResponseResult();
+        }
+
+        private readonly IOrganisationService _organisationService;
+    }
+}
