db70321daef2828475da98ad0ab86ffd99e38109
Aderibigbe Adeola <115571361+Adeola-Aderibigbe@users.noreply.github.com>
2023-10-10 15:20:08 +0100

    Set up db service

---
+++ KAMU.API/Infrastructure/Services/Db/DbService.cs
@@ -0,0 +1,45 @@
+﻿using KAMU.API.Infrastructure.Utilities;
+using ISession = NHibernate.ISession;
+
+namespace KAMU.API.Infrastructure.Services.Db
+{
+    /// <summary>
+    /// Manages the data manipulation
+    /// </summary>
+    public class DbService : IDbService
+    {
+        /// <summary>
+        /// A constructor
+        /// </summary>
+        /// <param name="session">Manages the transactions in the database</param>
+        public DbService(ISession session)
+        {
+            _session = session;
+        }
+
+        /// <summary>
+        /// Commit all activities to the database
+        /// </summary>
+        /// <returns>Action response</returns>
+        public virtual async Task<ActionResponse> CommitAsync()
+        {
+            using var transaction = _session.BeginTransaction();
+            try
+            {
+                await transaction.CommitAsync();
+                await Task.CompletedTask;
+            }
+            catch (Exception ex)
+            {
+                return ActionResponse.Failed(ex.Message);
+            }
+
+            return ActionResponse.Successful();
+        }
+
+        /// <summary>
+        /// Manages the transactions in the database
+        /// </summary>
+        private ISession _session;
+    }
+}
