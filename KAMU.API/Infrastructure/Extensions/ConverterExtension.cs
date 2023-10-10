7c08efb76c6019563271a3ff9aa167aaacc7feb7
Aderibigbe Adeola <115571361+Adeola-Aderibigbe@users.noreply.github.com>
2023-10-10 15:13:48 +0100

    Add custom mapper

---
+++ KAMU.API/Infrastructure/Extensions/ConverterExtension.cs
@@ -0,0 +1,34 @@
+﻿using KAMU.API.Domain.Models;
+
+namespace KAMU.API.Infrastructure.Extensions
+{
+    /// <summary>
+    /// Converts an object to another object
+    /// </summary>
+    public static class ConverterExtension
+    {
+        /// <summary>
+        /// Converts an object to another object
+        /// </summary>
+        /// <typeparam name="T">The destination type</typeparam>
+        /// <param name="entity">The source object</param>
+        /// <returns>Destination object</returns>
+        public static T ConvertTo<T>(this IEntity entity) where T: IEntity
+        {
+           T obj = (T)Activator.CreateInstance(typeof(T));
+
+           var properties =  obj.GetType().GetProperties();
+           var personProperties = entity.GetType().GetProperties();
+           for(int i = 0; i < properties.Length; i++)
+           {
+                var property = personProperties.Where(s => s.Name == properties[i].Name).FirstOrDefault();
+                if (property == null)
+                    continue;
+
+                var value = property.GetValue(entity, null);
+                properties[i].SetValue(obj,value);
+           }
+           return obj;
+        }
+    }
+}
